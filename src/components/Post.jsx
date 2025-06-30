8968a2a082ba947296cd789810b073d516f97432
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2025-06-30 14:05:46 +0300

    Added a search bar and enhanced the page visually

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,19 +3,37 @@ export default function Post({ username, content, groupName, date }) {
         flexDirection: "column",
         display: "flex",
         alignItems: "center",
-        fontSize:"2rem",
+        fontSize: "2rem",
+        backgroundColor: "var(--color-dark)",
+        color: "var(--color-light)",
+        padding: "20px",
+        width: "66%",
+        borderRadius: "15px",
     }}>
-        <div>
-            {username}
-        </div>
-        <div>
+        <div style={{
+            borderStyle: "solid",
+            borderColor: "var(--color-light)",
+            marginTop: "20px",
+            width: "100%",
+            padding: "17px",
+            borderRadius: "30px",
+            borderWidth: "1px",
+            fontFamily:"cursive"
+        }}>
             {content}
         </div>
-        <div>
-            {groupName}
-        </div>
-        <div>
-            {date}
+        <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            padding: "40px",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            textDecoration: "underline",
+            fontSize: "xx-large"
+        }}>
+            <div>User: {username}</div>
+            <div>Group: {groupName}</div>
         </div>
     </div>)
 }
