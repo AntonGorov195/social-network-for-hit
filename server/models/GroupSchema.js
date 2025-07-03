a127d7a42fcfe1e015b9368af9e8c02f20b1a925
shlomige <shlomi@geva4u.com>
2025-07-03 20:49:16 +0300

    add group schema

--- server/models/GroupSchema.js
+++ server/models/GroupSchema.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 
+const GroupSchame = new mongoose.Schema({
+    name: {type:String, required:true},
+    createdAt: {type:Date,default:Date.now},
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+    description: {type:String},
+})
+module.exports = mongoose.model("group", GroupSchame);
