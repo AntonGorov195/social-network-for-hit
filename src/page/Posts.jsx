3d2b4667e25f1555c1fedfc3e9c9aec5dd1b5296
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2025-07-03 20:16:18 +0300

    added client side group filtering in advance search.

--- src/page/Posts.jsx
+++ src/page/Posts.jsx
@@ -38,93 +38,153 @@ export default function Posts(props) {
 
     useEffect(() => {
         const fetchPosts = async () => {
+            setLoading(true);
             try {
                 const posts = await axios.get('http://localhost:5000/api/posts', {
                     params: {
-                        userid: "123",
+                        userid: "321321",
                     }
                 }).then(response => {
-                    setLoading(false);
-                    if (searchInput != "") {
-                        setPosts(response.data.filter((p) => {
-                            return p.PostContent.includes(searchInput)
-                        }));
-                    } else {
-                        setPosts(response.data);
-                    }
+                    setPosts(response.data.filter((p) => {
+                        const includes = p.PostContent.includes(searchInput);
+                        if (advancedSearchGroup != "") {
+                            return includes && p.PostGroup.includes(advancedSearchGroup);
+                        }
+                        return includes;
+                    }));
                 }).catch(error => {
                     console.error(error);
                 });
             }
             catch (error) {
                 console.error(error);
+            } finally {
+                setLoading(false);
             }
         }
         fetchPosts();
-    }, [searchInput])
+    }, [searchInput, advancedSearchGroup])
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            {loading ?
-                <div> Loading </div>
-                : <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px", width: "66%" }}>
-                    <div style={{
-                        width: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "3px",
-                        fontSize: "2rem",
-                        color: "var(--color-light)",
-                    }}>
-                        {/* <img src="https://img.icons8.com/metro/26/search.png" style={{
-                            padding: "10px",
-                            backgroundColor: "var(--color-dark)",
-                            borderRadius: "20px",
-                            fontSize: "2rem",
-                            color: "",
-
-                        }} /> */}
+        <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+            <main style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px", width: "66%" }}>
+                <form style={{ display: "flex", width: "100%", flexDirection: "column", lignItems: "center", gap: "10px" }} onSubmit={(e) => {
+                    e.preventDefault()
+                }}>
+                    <div style={{ display: "flex", gap: "10px" }}>
                         <PostsSearchBar onInput={(e) => setSearchInput(e.currentTarget.value)} />
                         <SearchIcon />
                         <AdvanceSearchButton onClick={(e) => setAdvancedSearchOn(!advancedSearchOn)} />
                     </div>
-                    {
-                        advancedSearchOn &&
-                        <div style={{
-                            backgroundColor: "var(--color-dark)",
-                            color: "var(--color-light)",
-                            width: "100%",
-                            fontSize: "1.5rem",
-                            padding: "10px",
-                            borderRadius: "20px",
-                        }}>
+                    <div>
+                        {
+                            advancedSearchOn &&
                             <div style={{
-                                display: "flex",
-                                gap: "3px",
+                                backgroundColor: "var(--color-dark)",
+                                color: "var(--color-light)",
+                                width: "100%",
+                                fontSize: "1.5rem",
+                                padding: "10px",
+                                borderRadius: "20px",
                             }}>
-                                <div> Group search</div>
-                                <input style={{
-                                    color: "var(--color-light)",
-                                    backgroundColor: "var(--color-dark)",
-                                    fontSize: "1.5rem",
-                                    flex: "1",
-                                }} />
+                                <div style={{
+                                    display: "flex",
+                                    gap: "3px",
+                                }}>
+                                    <div> Group search</div>
+                                    <input style={{
+                                        color: "var(--color-light)",
+                                        backgroundColor: "var(--color-dark)",
+                                        fontSize: "1.5rem",
+                                        flex: "1",
+                                    }} onInput={(e) => {
+                                        setAdvancedSearchGroup(e.currentTarget.value)
+                                    }} />
+                                </div>
+                                <div>
+                                    User Search
+                                </div>
+                                <div>
+                                    Another Filter
+                                </div>
                             </div>
-                            <div>
-                                User Search
-                            </div>
-                            <div>
-                                Another Filter
-                            </div>
-                        </div>
-                    }
-                    {
-                        posts.map((p) => {
-                            return <Post key={p.PostId} username={p.PostAuthor} content={p.PostContent} groupName={p.PostGroup} date={p.PostDate} />
-                        })
-                    }
-                </div>
-            }
+                        }
+                    </div>
+                </form>
+                {
+                    loading ?
+                        (<div style={{ fontSize: "6rem" }}>Loading</div>)
+                        : (
+                            posts.map((p) => {
+                                return <Post key={p.PostId} username={p.PostAuthor} content={p.PostContent} groupName={p.PostGroup} date={p.PostDate} />
+                            })
+                        )
+                }
+            </main>
         </div>
     )
+    //     return (
+    //         <div style={{ display: "flex", justifyContent: "center" }}>
+    //             {loading ?
+    //                 <div> Loading </div>
+    //                 : <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px", width: "66%" }}>
+    //                     <div style={{
+    //                         width: "100%",
+    //                         display: "flex",
+    //                         alignItems: "center",
+    //                         gap: "3px",
+    //                         fontSize: "2rem",
+    //                         color: "var(--color-light)",
+    //                     }}>
+    //                         {/* <img src="https://img.icons8.com/metro/26/search.png" style={{
+    //                             padding: "10px",
+    //                             backgroundColor: "var(--color-dark)",
+    //                             borderRadius: "20px",
+    //                             fontSize: "2rem",
+    //                             color: "",
+    // 
+    //                         }} /> */}
+    //                         <PostsSearchBar onInput={(e) => setSearchInput(e.currentTarget.value)} />
+    //                         <SearchIcon />
+    //                         <AdvanceSearchButton onClick={(e) => setAdvancedSearchOn(!advancedSearchOn)} />
+    //                     </div>
+    //                     {
+    //                         advancedSearchOn &&
+    //                         <div style={{
+    //                             backgroundColor: "var(--color-dark)",
+    //                             color: "var(--color-light)",
+    //                             width: "100%",
+    //                             fontSize: "1.5rem",
+    //                             padding: "10px",
+    //                             borderRadius: "20px",
+    //                         }}>
+    //                             <div style={{
+    //                                 display: "flex",
+    //                                 gap: "3px",
+    //                             }}>
+    //                                 <div> Group search</div>
+    //                                 <input style={{
+    //                                     color: "var(--color-light)",
+    //                                     backgroundColor: "var(--color-dark)",
+    //                                     fontSize: "1.5rem",
+    //                                     flex: "1",
+    //                                 }} />
+    //                             </div>
+    //                             <div>
+    //                                 User Search
+    //                             </div>
+    //                             <div>
+    //                                 Another Filter
+    //                             </div>
+    //                         </div>
+    //                     }
+    //                     {
+    //                         posts.map((p) => {
+    //                             return <Post key={p.PostId} username={p.PostAuthor} content={p.PostContent} groupName={p.PostGroup} date={p.PostDate} />
+    //                         })
+    //                     }
+    //                 </div>
+    //             }
+    //         </div>
+    //     )
 }
