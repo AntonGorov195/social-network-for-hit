60f748a3e3012d678a11c095897fa30133f17d31
shlomige <shlomi@geva4u.com>
2025-07-04 17:30:03 +0300

    add all users and some users search2

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserSchema');
 const jwt = require('jsonwebtoken');
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 
 router.post('/create', async (req, res) => {
+    console.log("trying to create new user");
     try {
         const username = req.body.username;
         const password = req.body.password;
@@ -12,20 +15,21 @@ router.post('/create', async (req, res) => {
         const createdAt = req.body.createdAt ? new Date(req.body.createdAt) : undefined;
         if (!username || !password || !email) {
             res.status(400).send('missing required parameters');
-        }else{
-            const existingUser = await User.findOne({username:username});
+        } else {
+            const existingUser = await User.findOne({username: username});
             if (existingUser) {
                 console.log('user already exists');
                 res.status(400).send('user already exists');
-            }else {
-                const newUser = new User({username, password,email,createdAt});
+            } else {
+                const newUser = new User({username, password, email, createdAt});
                 await newUser.save();
                 res.status(201).json({message: 'successfully created user'});
+                console.log("user created successfully");
+            }
         }
-        }
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({error: 'Server error'});
     }
 });
 
@@ -35,18 +39,52 @@ router.post('/login', async (req, res) => {
     const password = req.body.password;
     if (!username || !password) {
         res.status(400).send('missing required parameters');
-    }else {
-        const existingUser = await User.findOne({username:username});
+    } else {
+        const existingUser = await User.findOne({username: username});
         if (!existingUser || existingUser.password !== password) {
             res.status(401).send('invalid username or password');
-        }
-        else {
-            const token = jwt.sign({username:username},process.env.JWT_SECRET,{expiresIn:'7d'});
-            res.json({'token':token});
+        } else {
+            const token = jwt.sign({username: username}, process.env.JWT_SECRET, {expiresIn: '7d'});
+            res.json({'token': token});
             res.status(200).send("logged in successfully");
         }
     }
 })
 
 
+router.get('/getAllUsers', async (req, res) => {
+    console.log('trying to get all users');
+    try {
+        const users = await User.find({});
+        res.status(200).json({
+            message: "users retrieved successfully",
+            data: users
+        })
+        console.log("users retrieved successfully");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "something went wrong",
+            error: err
+        });
+    }
+});
+
+router.get('getUsersByParameters', async (req, res) => {
+const {username, email, createdAt} = req.query;
+try {
+    let query = {};
+    if (username) query.username = username;
+    if (email) query.email = email;
+    if (createdAt) query.createdAt = createdAt;
+    //const users = await User.find(query);
+    res.status(200).json({users: users})
+} catch (err) {
+    console.log(err);
+    res.status(500).json({
+        message: "something went wrong",
+        error: err
+    });
+}
+})
 module.exports = router;
