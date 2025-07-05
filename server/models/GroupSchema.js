8dbf4eccbdb8d20f784346d527db7431a08cb5c3
shlomige <shlomi@geva4u.com>
2025-07-05 22:23:41 +0300

    add update and delete group and add a manager to group schema

--- server/models/GroupSchema.js
+++ server/models/GroupSchema.js
@@ -5,5 +5,7 @@ const GroupSchame = new mongoose.Schema({
     createdAt: {type:Date,default:Date.now},
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
     description: {type:String},
+    lastUpdatedAt: {type:Date, default:Date.now},
+    managerUser : {type : mongoose.Schema.Types.ObjectId,ref: 'User',required:true}
 })
 module.exports = mongoose.model("group", GroupSchame);
--- server/routes/groupRoute.js
+++ server/routes/groupRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Group = require("../models/GroupSchema");
+const mongoose = require("mongoose");
 
 router.get("/getAllGroups", async (req, res) => {
     console.log("trying to get all groups");
@@ -16,7 +17,8 @@ router.get("/getAllGroups", async (req, res) => {
         res.status(500).json({
             message: 'Something went wrong',
             error: err
-        })
+        });
+        console.error(err);
     }
 
 })
@@ -26,8 +28,9 @@ router.post("/createGroup", async (req, res) => {
         const name = req.body.name;
         const description = req.body.description;
         const members = req.body.members;
+        const manageruser = req.body.managerUser
         const createdAt = req.body.createdAt ? new Date(req.body.createdAt) : undefined
-        if (!name || !Array.isArray(members) || members.length === 0) {
+        if (!name || !Array.isArray(members) || members.length === 0 || !manageruser) {
             res.status(400).send('missing required parameters');
         }
         else {
@@ -35,7 +38,8 @@ router.post("/createGroup", async (req, res) => {
                 name: name,
                 description: description,
                 members: members,
-                createdAt: createdAt
+                createdAt: createdAt,
+                managerUser: (manageruser)
             });
             const savedGroup = await newGroup.save();
             res.status(201).json({message: 'Group created successfully', data: savedGroup});
@@ -47,8 +51,55 @@ router.post("/createGroup", async (req, res) => {
             message: 'Something went wrong',
             error: err
         });
+        console.log(err)
 
     }
 
 })
+router.put("/updateGroup", async (req, res) => {
+    console.log("trying to update a group");
+    const name = req.body.name;
+    if (!name){
+        res.status(400).send('missing required parameters');
+    }else {
+        try {
+            const updatedGroup = await Group.findOneAndUpdate(
+            {name: name},
+            req.body,
+            {new: true}
+            )
+            if (!updatedGroup) {
+                res.status(404).send("group not found");
+            }
+            else {
+                res.status(200).json({message: 'Group updated successfully', data: updatedGroup});
+                console.log("Group updated successfully");
+            }
+        }catch (error) {
+            res.status(500).json({
+                message: 'Something went wrong',
+                error: error
+            });
+            console.log(error);
+        }
+    }
+
+});
+router.delete("/deleteGroup", async (req, res) => {
+    console.log("trying to delete a group");
+    const name = req.body.name;
+    try {
+        const deletedGroup = await Group.findOneAndDelete({name:name});
+        if (!deletedGroup) {
+            res.status(404).send("group not found");
+        }else {
+            res.status(200).json({message: 'Group deleted successfully', data: deletedGroup});
+            console.log("Group deleted successfully");
+        }
+    }catch(err) {
+        res.status(500).json({message: 'Something went wrong', error: err});
+        console.log(err);
+    }
+})
+
 module.exports = router;
