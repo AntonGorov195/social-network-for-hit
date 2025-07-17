3c3bb8a51ddcfde3127b22573ffe809a16198daa
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2025-07-17 21:11:46 +0300

    moved socket logic to a different file

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const http = require("http");
-const socketIo = require("socket.io");
-const Messages = require("./models/MessagesSchema");
-const authMiddleware = require("./middlewares/authMiddleware");
-const User = require("./models/UserSchema");
-const Chats = require("./models/ChatsSchema");
+const InitSocket = require("./socket/socket");
 
 const app = express();
 app.use(cors());
@@ -43,79 +39,7 @@ app.post("/api/items", async (req, res) => {
 });
 //
 const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: { origin: "http://localhost:3000" },
-});
-// io.on("connection", (socket) => {
-//     socket.on("chat message", async (data) => {
-//         const jwt = require("jsonwebtoken");
-//         const JWT_SECRET = process.env.JWT_SECRET;
-//         try {
-//             const token = data.token;
-//             const decoded = jwt.verify(token, JWT_SECRET);
-//             const userId = decoded.userId;
-//             chatId = data.chatId;
-//             const msg = {
-//                 text: data.text,
-//                 sender: userId,
-//             };
-//             await Chats.findByIdAndUpdate(chatId, {
-//                 $push: {
-//                     messages: msg,
-//                 },
-//             });
-//             io.emit("chat message", {
-//                 message: msg,
-//                 chatId: data.chatId,
-//             });
-//         } catch (e) {
-//             console.error(e);
-//         }
-//     });
-// });
-io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    // Join room for a specific chat
-    socket.on("join-chat", (chatId) => {
-        console.log(`join-chat ${chatId}`);
-        socket.join(chatId);
-    });
-
-    // Receive message and broadcast to chat room
-    socket.on("send-message", async ({ chatId, text, token }) => {
-        const jwt = require("jsonwebtoken");
-        const JWT_SECRET = process.env.JWT_SECRET;
-        try {
-            const decoded = jwt.verify(token, JWT_SECRET);
-            const userId = decoded.userId;
-            const user = await User.findById(userId);
-            console.log(`send-message ${text}, from ${user.username}`);
-            io.to(chatId).emit("receive-message", {
-                text: text,
-                sender: {
-                    username: user.username,
-                    _id: userId,
-                },
-            });
-            const msg = {
-                text: text,
-                sender: userId,
-            };
-            await Chats.findByIdAndUpdate(chatId, {
-                $push: {
-                    messages: msg,
-                },
-            });
-        } catch (e) {
-            console.error(e);
-        }
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
-    });
-});
+InitSocket(server)
 // Connect to MongoDB
 mongoose
     .connect(process.env.MONGO_URI, {})
